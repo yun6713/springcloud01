9bdae864bf77f0d358d57b474af509c957b4a31d
litianlin <litianlin@bonc.com.cn>
2019-04-17 22:51:23 +0800

    2019年4月17日22:51:20

--- eureka-zuul/src/main/java/eurekaClient/EurekaZuul.java
+++ eureka-zuul/src/main/java/eurekaClient/EurekaZuul.java
@@ -1,12 +1,15 @@
 package eurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy//eureka client
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaZuul {
 	public static void main(String[] args) {
+		org.springframework.cloud.netflix.zuul.ZuulServerAutoConfiguration a;
 		SpringApplication.run(EurekaZuul.class,args);
 	}
 }
